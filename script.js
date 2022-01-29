abb20c3e44e0a4bec8bc508d220a3afee9b2c000
Kuba <kubapilat80@gmail.com>
2022-01-29 13:11:19 +0100

    add validation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const number = document.getElementById("drawnNumber").value;
 
 function oblicz() {
+    const number = document.getElementById("drawnNumber").value;
     document.getElementById("bigNumberDisplay").innerHTML = number;
     document.getElementById(number).style.backgroundColor  = "#1C67BA";
     document.getElementById(number).style.color  = "#F1FDFD";
