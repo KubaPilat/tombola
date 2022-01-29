d022db59206998dac9f4ba43cf88a12bb23451ca
Kuba <kubapilat80@gmail.com>
2022-01-29 17:20:34 +0100

    add validation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function oblicz() {
     document.getElementById("bigNumberDisplay").innerHTML = number;
     document.getElementById(number).style.backgroundColor = "#1C67BA";
     document.getElementById(number).style.color = "#F1FDFD";
-    if (number === "" || number >= 91 || number <= 0) {
+    if (number == " " || number >= 91 || number <= 0) {
         document.getElementById("Error").innerHTML = "Błędne wpisanie liczby";
     }
 }
